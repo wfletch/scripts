be6dd79abdaa6a793ef96a4e86ca34ccec5314e7
Warren Fletcher <warren@fletch.band>
2022-03-06 16:47:28 +0100

    Working Out Issues with Getting Todays Date

--- update_configs.pl
+++ update_configs.pl
@@ -2,18 +2,20 @@
 use v5.10;
 use warnings;
 use strict;
+use POSIX qw(strftime);
 # WIP: The idea here is to update all my config repositories automatically everyday 
 # Ok, what are we doing.
 # 1. Copy Over Configs to the config repo
  
 say "Switching Directories to: ", "$ENV{'BACKUP_CONFIG_DIR'}";
 chdir "$ENV{'BACKUP_CONFIG_DIR'}";
+my $date = strftime "%m/%d/%Y", localtime;
 say "Copying Configs";
 my $output = `./get_configs.sh`;
-say "Updating Configs";
+say "Updating Configs: $date";
 `git add *`;
-my $date_string = localtime();
-`git commit -m "Auto Update: $date_string"`;
+`echo $date >> date_entry`;
+`git commit -m "Auto Update: $date"`;
 `git pull --rebase`;
 `git push`;
 say "All Done";
