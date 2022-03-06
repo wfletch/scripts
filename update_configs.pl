836bc4592399ec8c38a25102d49515f4ec937b83
Warren Fletcher <warren@fletch.band>
2022-03-06 16:37:25 +0100

    Working Automatic Crontab for scripts

--- update_configs.pl
+++ update_configs.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/perl
+use v5.10;
 use warnings;
 use strict;
-
-# WIP: The idea here is to update all my config repositories automatically everyday.
+# WIP: The idea here is to update all my config repositories automatically everyday 
+# Ok, what are we doing.
+# 1. Copy Over Configs to the config repo
+ 
+say "Switching Directories to: ", "$ENV{'BACKUP_CONFIG_DIR'}";
+chdir "$ENV{'BACKUP_CONFIG_DIR'}";
+say "Copying Configs";
+my $output = `./get_configs.sh`;
+say "Updating Configs";
+`git add *`;
+my $date_string = localtime();
+`git commit -m "Auto Update: $date_string"`;
+`git pull --rebase`;
+`git push`;
+say "All Done";
